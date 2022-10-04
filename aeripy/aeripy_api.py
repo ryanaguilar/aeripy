d11b3410f30b1921d0908058f6887b35d7d8eef1
Ryan Aguilar <ryanaguilar@Ryans-MacBook-Pro.local>
2022-10-04 07:11:52 -0700

    snake case all the keys

--- aeripy/aeripy_api.py
+++ aeripy/aeripy_api.py
@@ -5,7 +5,7 @@ from aeripy.exceptions import AeripyException
 from aeripy.models import *
 from .endpoints import API_PATH
 from .models import SystemInfo, School
-
+from .util.snake import snake_case_keys
 
 class AeripyApi:
     def __init__(self, hostname: str = "demo.aeries.net/aeries/api", api_key: str = '477abe9e7d27439681d62f4e0de1f5e1',
@@ -16,22 +16,22 @@ class AeripyApi:
 
     def get_system_info(self) -> SystemInfo:
         result = self._rest_adapter.get(endpoint=API_PATH["system_info"])
-        sys_info: SystemInfo = SystemInfo(**result.data)
+        sys_info = SystemInfo(**snake_case_keys(result.data))
         return sys_info
 
     def get_schools(self) -> List[School]:
         result = self._rest_adapter.get(endpoint=API_PATH['schools'])
-        schools_list: List[School] = [School(**datum) for datum in result.data]
+        schools_list = [School(**snake_case_keys(datum)) for datum in result.data]
         return schools_list
 
     def get_school(self, school_code: int) -> School:
         result = self._rest_adapter.get(endpoint=API_PATH['school'].format(school_code=school_code))
-        school: School = School(**result.data)
+        school = School(**snake_case_keys(result.data))
         return school
 
     def get_terms(self, school_code: int) -> List[Term]:
         result = self._rest_adapter.get(endpoint=API_PATH['terms'].format(school_code=school_code))
-        terms_list: List[Term] = [Term(**datum) for datum in result.data]
+        terms_list = [Term(**snake_case_keys(datum)) for datum in result.data]
         return terms_list
 
     def get_bell_schedules(self, school_code: str, date: int=None) -> List[BellScheduleElement]:
@@ -40,12 +40,12 @@ class AeripyApi:
         else:
             endpoint = API_PATH['bell_schedule'].format(school_code=school_code)
         result = self._rest_adapter.get(endpoint=endpoint)
-        bell_schedules = [BellScheduleElement(**datum) for datum in result.data]
+        bell_schedules = [BellScheduleElement(**snake_case_keys(datum)) for datum in result.data]
         return bell_schedules
 
     def get_bell_schedule(self, school_code: str, date: int) -> BellScheduleElement:
         result = self.get_bell_schedules(school_code, date)
-        bell_schedule = BellScheduleElement(**result.data)
+        bell_schedule = BellScheduleElement(**snake_case_keys(result.data))
         return bell_schedule
 
 
