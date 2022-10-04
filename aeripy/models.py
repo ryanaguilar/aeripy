9c612b74dabce23c35d0de2c10c4a80512291f61
raguilar <raguilar@scoe.org>
2022-10-04 12:59:51 -0700

    add Staff model

--- aeripy/models.py
+++ aeripy/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union, Optional
 from datetime import datetime
 from requests.structures import CaseInsensitiveDict
 from enum import Enum
@@ -162,3 +162,60 @@ class AbsenceCodeElement:
     count_on_report_card: bool
     is_suspension: bool
     is_partial_day_truant: bool
+
+
+@dataclass
+class SchoolAccessPermission:
+    school_code: int
+    read_only_access: bool
+    communication_group: bool
+
+
+@dataclass
+class StaffElement:
+    school_access_permissions: List[SchoolAccessPermission]
+    extended_properties: List[Any]
+    early_childhood_certification_code: str
+    gender: str
+    education_level_code: Union[InactiveStatusCode, int]
+    ethnicity_code: EthnicityCode
+    race_code1: str
+    race_code2: str
+    race_code3: str
+    race_code4: str
+    race_code5: str
+    position_status_code: str
+    total_years_of_edu_service: int
+    total_years_in_this_district: int
+    previous_last_name: str
+    previous_first_name: str
+    previous_middle_name: str
+    name_suffix: str
+    address: str
+    address_city: str
+    address_state: str
+    address_zip_code: int
+    address_zip_ext: int
+    home_phone: str
+    emergency_contact_name: str
+    emergency_contact_phone: str
+    id: int
+    first_name: str
+    last_name: str
+    middle_name: str
+    birth_year: int
+    full_time_percentage: int
+    inactive_status_code: str
+    state_educator_id: str
+    user_name: str
+    email_address: str
+    primary_aeries_school: int
+    network_login_id: str
+    alternate_email_address: str
+    human_resources_system_id: str
+    cell_phone: str
+    notification_preference_code: str
+    title: str
+    birth_date: Optional[datetime] = None
+    hire_date: Optional[datetime] = None
+    leave_date: Optional[datetime] = None
