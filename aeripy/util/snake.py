c769c3adf1af610ac62e55046e5232b530e0f5ae
raguilar <raguilar@scoe.org>
2022-10-05 11:29:41 -0700

    add snake_to_camel util function

--- aeripy/util/snake.py
+++ aeripy/util/snake.py
@@ -7,6 +7,9 @@ _re_camel_to_snake = re.compile(r"([a-z0-9](?=[A-Z])|[A-Z](?=[A-Z][a-z]))")
 def camel_to_snake(name: str) -> str:
     return _re_camel_to_snake.sub(r"\1_", name).lower()
 
+def snake_to_camel(name: str) -> str:
+    return ''.join([word.replace(word[0], word[0].upper()) for word in name.split('_')])
+
 
 def snake_case_keys(dictionary: Dict[str, Any]) -> Dict[str, Any]:
     return {camel_to_snake(k): v for k, v in dictionary.items()}
