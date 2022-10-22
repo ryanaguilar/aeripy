0ecc3fde7cc371015114d363e4d8cc0e014ecdfc
Ryan Aguilar <ryanaguilar323@gmail.com>
2022-10-22 11:59:34 -0700

    add betamax imports to conftest.py.  Add betamax to requirements.txt

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
 """Prepare py.test."""
+import json
+import os
+
+import betamax
+import pytest
+from betamax.cassette.cassette import Cassette, dispatch_hooks
+from betamax.serializers import JSONSerializer
+
