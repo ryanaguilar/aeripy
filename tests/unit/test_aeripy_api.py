9c2dd137610de445f6670a0c76b8f8a077391e21
Ryan Aguilar <ryanaguilar323@gmail.com>
2022-10-29 14:29:17 -0700

    add seid too long test

--- tests/unit/test_aeripy_api.py
+++ tests/unit/test_aeripy_api.py
@@ -16,7 +16,7 @@ class TestAeripyApi(TestCase):
     def test_get_one_staff_id_only(self):
         self.aeripyapi._rest_adapter.get.return_value = Result(status_code=200,
                                                                headers={},
-                                                               data={"ID": 1}
+                                                               data={"Id": 1}
                                                                )
         staff = self.aeripyapi.get_staff(1)
         assert isinstance(staff, StaffElement)
@@ -47,9 +47,18 @@ class TestAeripyApi(TestCase):
             self.aeripyapi._rest_adapter.get.return_value = Result(status_code=200,
                                                                    headers={},
                                                                    data=[{"NameFirst": 'Joe'}]
-
-                                                                   )
+                                                     )
             self.aeripyapi.get_staff()
 
+    def test_get_staff_seid_too_long(self):
+        with pytest.raises(ValidationError):
+            self.aeripyapi._rest_adapter.get.return_value = Result(status_code=200,
+                                                               headers={},
+                                                               data={
+                                                                        "Id": 1,
+                                                                        "StateEducatorId": "12345678901",
+                                                                     }
+                                                               )
+            self.aeripyapi.get_staff(1)
 
 
