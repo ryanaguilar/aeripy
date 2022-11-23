a296198474a2287ce778dd825ee07a056c67734c
raguilar <raguilar@scoe.org>
2022-11-23 09:29:36 -0800

    add FlexPeriod model

--- aeripy/models/scheduling.py
+++ aeripy/models/scheduling.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from datetime import datetime
 
 @dataclass
 class ClassCalendar:
@@ -8,3 +8,15 @@ class ClassCalendar:
     academic_year: str
     short_title: str
     description: str
+
+
+@dataclass
+class FlexPeriod:
+    sequence_number: int
+    school_code: int
+    academic_year: str
+    short_title: str
+    description: str
+    start_time: datetime
+    end_time: datetime
+    type_code: str
